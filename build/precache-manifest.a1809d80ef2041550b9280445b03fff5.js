48f6ed6aa24baf8eb49597a6d742859a9ffc0958
Hamish Harrison <57675214+Noxirus@users.noreply.github.com>
2020-07-27 10:36:40 -0600

    Added Recent website build

---
+++ build/precache-manifest.a1809d80ef2041550b9280445b03fff5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea811ae5b03c28120a4a3fc2e66deb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f45f9d35176f4fd1f167",
+    "url": "/static/css/2.23e836e8.chunk.css"
+  },
+  {
+    "revision": "087ab174fa8d7ef32864",
+    "url": "/static/css/main.16f079fd.chunk.css"
+  },
+  {
+    "revision": "f45f9d35176f4fd1f167",
+    "url": "/static/js/2.4cf97e7f.chunk.js"
+  },
+  {
+    "revision": "c560b744118e776c5a9fe14e0546e12a",
+    "url": "/static/js/2.4cf97e7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "087ab174fa8d7ef32864",
+    "url": "/static/js/main.fbafd72e.chunk.js"
+  },
+  {
+    "revision": "55c4aa5effadd9d5d067",
+    "url": "/static/js/runtime-main.d1426e2b.js"
+  },
+  {
+    "revision": "1ed87fd8bcb82c1d64f64666376be530",
+    "url": "/static/media/About.1ed87fd8.png"
+  },
+  {
+    "revision": "0efe9e2ab1f3bdea1d250af15ecd59be",
+    "url": "/static/media/Carousel1.0efe9e2a.jpg"
+  },
+  {
+    "revision": "291d3dec96dd081cf5701bda8f4e4d35",
+    "url": "/static/media/Carousel2.291d3dec.jpg"
+  },
+  {
+    "revision": "b11f7d05f50d7b2b7bbfb8200df5a063",
+    "url": "/static/media/Carousel3.b11f7d05.jpg"
+  },
+  {
+    "revision": "26eaf9af9fc4df8aa3ff97fb51718eea",
+    "url": "/static/media/Chicky.26eaf9af.png"
+  },
+  {
+    "revision": "e2c8826eb90d6fbb621304aae9152ad9",
+    "url": "/static/media/Hamlicks_LOGOCropped.e2c8826e.png"
+  },
+  {
+    "revision": "92500dbc9a650636decb9ab6628dad6a",
+    "url": "/static/media/Hamlicks_TITLE.92500dbc.png"
+  },
+  {
+    "revision": "0ff150a4d2c943d46ca21266da5b7fcc",
+    "url": "/static/media/Minty.0ff150a4.png"
+  },
+  {
+    "revision": "8b35c35c44c9b80fe9abfd97bde31873",
+    "url": "/static/media/Piggy.8b35c35c.png"
+  },
+  {
+    "revision": "648321eaf00ba092a50ec06667c8549c",
+    "url": "/static/media/Strawpiggy.648321ea.png"
+  }
+]);
